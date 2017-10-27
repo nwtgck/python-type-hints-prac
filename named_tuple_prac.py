0d5c1ea0dacace215b97d1e169b0ef8b713958db
Ryo Ota <nwtgck@gmail.com>
2017-10-27 11:39:53 +0900

    [add] Add NamedTuple practice

---
+++ named_tuple_prac.py
@@ -0,0 +1,24 @@
+import typing
+
+# NOTE: Use 3.6.3 to use syntax bellow
+# class Person(typing.NamedTuple):
+#     name: str
+#     age : int
+
+
+Person = typing.NamedTuple("Person", [('name', str), ('age', int)]) # (from: https://stackoverflow.com/a/34269877/2885946)
+
+
+def f1(person: Person) -> int:
+    return person.age
+
+def f2(i: int) -> None:
+    pass
+
+
+person = Person(name="jack", age=4)
+print(f1(person))
+# print(f1(2)) # mypy error(GOOD!): error: Argument 1 to "f1" has incompatible type "int"; expected "Person"
+
+
+print(f2(person.age))
